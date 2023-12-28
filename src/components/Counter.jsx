9c5248e264f9b2ce3f91ad59ce82679ae90b04ad
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2023-12-29 00:23:11 +0600

    increment and decrement value make dynamic

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -18,13 +18,13 @@ function Counter() {
       <div className="flex space-x-3">
         <button
           className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-          onClick={incrementHandler}
+          onClick={() => incrementHandler(5)}
         >
           Increment
         </button>
         <button
           className="bg-red-400 text-white px-3 py-2 rounded shadow"
-          onClick={decrementHandler}
+          onClick={() => decrementHandler(10)}
         >
           Decrement
         </button>
