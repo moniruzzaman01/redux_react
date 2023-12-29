dbb37e82593560d9f2d130cc9d34dfeb7d07c5f4
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2023-12-30 00:17:38 +0600

    redux-devtools installed to debug the web app

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./rootReducer";
 import myLogger from "./middlewares/myLogger";
 import reduxLogger from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(rootReducer, applyMiddleware(reduxLogger, myLogger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxLogger, myLogger))
+);
 
 export default store;
